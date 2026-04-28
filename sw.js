713ec4ff930caaf9a069d31d5da5783b4bc84697
phunk-pentium-one <diditseptian220@gmail.com>
2026-04-28 10:43:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
+const CACHE_NAME = "soto-it-v1";
+const assets = [
+  "./",
+  "index.html",
+  "css/style.css",
+  "js/script.js",
+  "img/soto.png",
+];
+
+// Tahap Install: Menyimpan file ke memori HP/Laptop
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    }),
+  );
+});
+
+// Tahap Fetch: Mengambil data dari memori kalau internet mati
 self.addEventListener("fetch", (event) => {
-  // Ini membiarkan aplikasi berjalan normal lewat internet
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
 });
